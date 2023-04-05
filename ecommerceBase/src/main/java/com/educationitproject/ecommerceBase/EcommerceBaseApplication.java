16d015ae4edd05ce1b8b2356e04f29400aeea8a1
Jorge Vidal Casa <jlvidal_2hotmail.com>
2023-04-05 17:35:46 -0500

    Creacion del proyecto base en Springboot

---
+++ ecommerceBase/src/main/java/com/educationitproject/ecommerceBase/EcommerceBaseApplication.java
@@ -0,0 +1,13 @@
+package com.educationitproject.ecommerceBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBaseApplication.class, args);
+	}
+
+}
---
+++ ecommerceBase/src/test/java/com/educationitproject/ecommerceBase/EcommerceBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educationitproject.ecommerceBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
